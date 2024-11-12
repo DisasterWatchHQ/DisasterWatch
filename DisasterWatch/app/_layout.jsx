1bc06c35b262ef2619cd862f16157bc657f9b130
5yndr0m <sdilanjana21@gmail.com>
2024-11-12 20:44:55 +0530

    refactor: migrate from typescript to javascript in expo app

---
+++ DisasterWatch/app/_layout.jsx
@@ -0,0 +1,26 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { Slot } from 'expo-router';
+
+const RootLayout = () => {
+  return (
+    <>
+      <Text> Header </Text>
+        <Slot />
+      <Text> Footer </Text>
+    </>
+  )
+};
+
+export default RootLayout;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    display: 'flex',
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
