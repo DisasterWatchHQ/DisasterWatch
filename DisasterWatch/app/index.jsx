1c416e29d1ade413ef9f6533d28cf7ba1ff8d80d
5yndr0m <sdilanjana21@gmail.com>
2024-11-13 23:33:05 +0530

    feat: created a button component

--- DisasterWatch/app/index.jsx
+++ DisasterWatch/app/index.jsx
@@ -1,17 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import { ScrollView, Text, View } from 'react-native';
-import { Link } from 'expo-router';
+import { Redirect, router } from 'expo-router';
 import '../global.css';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import  CustomButton  from '../components/customButton';
 
 export default function App() {
   return (
     <SafeAreaView className="bg-teal-700 h-full">
       <ScrollView contentContainerStyle={{ height: '100%' }}>
-        <View className="w-full justify-center items-center h-full px-4">
+        <View className="w-full justify-center items-center min-h-[85vh] px-4">
           <Text className="text-4xl font-extrabold">Disaster Watch</Text>
+          <CustomButton 
+            title="Get Started"
+            handlePress={() => router.push('/signIn')}
+            containerStyles="w-full mt-7 rounded-xl"
+          />
         </View>
       </ScrollView>
+      <StatusBar style="light" backgroundColor="teal" />
     </SafeAreaView>
   );
 }
