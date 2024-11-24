919184b4c0607df6c129032bd4d87ac30eebf9c0
5yndr0m <sdilanjana21@gmail.com>
2024-11-24 19:19:53 +0530

    feat: adding google map functionality
    added google maps successfully to the app with requesting users for
    permision. added the api-key injector script to inject googlemaps apikey

--- DisasterWatch/app/(tabs)/home.jsx
+++ DisasterWatch/app/(tabs)/home.jsx
@@ -1,11 +1,14 @@
 import { Text, View, ScrollView } from 'react-native';
 import React from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import Appheader from '../../components/appheader';
 
 const Home = () => {
   return (
     <SafeAreaView className="bg-teal-600 h-full">
-      <Text>home</Text>
+      <Appheader />
+      <ScrollView>
+      </ScrollView>
     </SafeAreaView>
   )
 };
