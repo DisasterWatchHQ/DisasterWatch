8b9170639e6ee912f3f8614db4a8cdfd238d9d98
Ravisara Jayasundara <139541406+RavisaraJayasundara@users.noreply.github.com>
2024-11-30 09:44:48 +0530

    Feat: Cleaning up landing page

--- DisasterWatch/app/index.jsx
+++ DisasterWatch/app/index.jsx
@@ -8,50 +8,25 @@ import TitleText from "../components/titleText";
 
 export default function App() {
   return (
-    <SafeAreaView className="bg-teal-700 h-full">
+    <SafeAreaView className="bg-neutral-800 h-full">
       <ScrollView contentContainerStyle={{ height: `[85vh]` }}>
         <View className="w-full justify-center items-center min-h-[85vh] px-4">
           <TitleText
             title="Welcome to"
-            containerStyles="text-white text-2xl font-bold"
+            containerStyles="text-neutral-100 text-2xl font-bold"
           />
           <TitleText
             title="Disaster Watch"
-            containerStyles="text-4xl font-extrabold"
+            containerStyles="text-4xl font-extrabold text-neutral-100"
           />
           <CustomButton
             title="Get Started"
             handlePress={() => router.push("/signIn")}
-            containerStyles="w-full mt-7 rounded-xl bg-yellow-700"
-          />
-          <CustomButton
-            title="To Home"
-            handlePress={() => router.push("/home")}
-            containerStyles="w-full mt-7 rounded-xl bg-yellow-700"
-          />
-          <CustomButton
-            title="To Dashboard"
-            handlePress={() => router.push("/Dashboard")}
-            containerStyles="w-full mt-7 rounded-xl bg-yellow-700"
-          />
-          <CustomButton
-            title="To Landing Page"
-            handlePress={() => router.push("/Landingpage")}
-            containerStyles="w-full mt-7 rounded-xl bg-yellow-700"
-          />
-          <CustomButton
-            title="To Detailed alerts"
-            handlePress={() => router.push("/DetailedAlert")}
-            containerStyles="w-full mt-7 rounded-xl bg-yellow-700"
-          />
-          <CustomButton
-            title="to Guide page"
-            handlePress={() => router.push("/Guide")}
-            containerStyles="w-full mt-7 rounded-xl bg-yellow-700"
+            containerStyles="w-full mt-7 rounded-xl bg-neutral-300"
           />
         </View>
       </ScrollView>
-      <StatusBar style="light" backgroundColor="teal" />
+      <StatusBar style="light" backgroundColor="neutral-800" />
     </SafeAreaView>
   );
 }
