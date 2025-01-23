806c20d5406e06dda2cfba697e163e9375b59fc7
Dinuki85 <prarthanadinuki2@gmail.com>
2025-01-23 07:37:31 +0530

    feat: integrate custom Poppins fonts into RootLayout

--- DisasterWatch/app/_layout.jsx
+++ DisasterWatch/app/_layout.jsx
@@ -12,3 +12,16 @@ import {
 } from "react-native-paper";
 import * as Notifications from "expo-notifications";
 import Geocoder from "react-native-geocoding";
+
+const RootLayout = () => {
+  const [fontsLoaded, error] = useFonts({
+    "Poppins-Regular": require("../assets/fonts/Poppins-Regular.ttf"),
+    "Poppins-Black": require("../assets/fonts/Poppins-Black.ttf"),
+    "Poppins-Bold": require("../assets/fonts/Poppins-Bold.ttf"),
+    "Poppins-ExtraBold": require("../assets/fonts/Poppins-ExtraBold.ttf"),
+    "Poppins-ExtraLight": require("../assets/fonts/Poppins-ExtraLight.ttf"),
+    "Poppins-Light": require("../assets/fonts/Poppins-Light.ttf"),
+    "Poppins-Medium": require("../assets/fonts/Poppins-Medium.ttf"),
+    "Poppins-SemiBold": require("../assets/fonts/Poppins-SemiBold.ttf"),
+    "Poppins-Thin": require("../assets/fonts/Poppins-Thin.ttf"),
+  });
