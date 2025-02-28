016da867b4426116f326fd9a182257e64c68bb99
5yndr0m <sdilanjana21@gmail.com>
2025-02-28 16:48:24 +0530

    feat: add dashboard api calls

---
+++ DisasterWatch/services/dash.js
@@ -0,0 +1,29 @@
+import axios from 'axios';
+import * as SecureStore from 'expo-secure-store';
+
+const api = axios.create({
+  baseURL: process.env.REACT_APP_API_URL,
+});
+
+// Add a request interceptor to add the auth token
+api.interceptors.request.use(
+  async (config) => {
+    try {
+      const session = await SecureStore.getItemAsync('userSession');
+      if (session) {
+        const { token } = JSON.parse(session);
+        if (token) {
+          config.headers.Authorization = `Bearer ${token}`;
+        }
+      }
+      return config;
+    } catch (error) {
+      return Promise.reject(error);
+    }
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+export default api;
