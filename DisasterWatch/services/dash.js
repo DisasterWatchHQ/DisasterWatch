90727755d33b9a321dcf616de40a9d440057def0
5yndr0m <sdilanjana21@gmail.com>
2025-02-28 21:21:43 +0530

    fix: add content type

--- DisasterWatch/services/dash.js
+++ DisasterWatch/services/dash.js
@@ -3,6 +3,9 @@ import * as SecureStore from 'expo-secure-store';
 
 const api = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
 });
 
 // Add a request interceptor to add the auth token
